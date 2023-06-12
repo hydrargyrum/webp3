fbb578c4a09eed219b6a373be99936ca3971c119
Hg <dev@indigo.re>
2023-06-12 22:40:31 +0200

    accept .opus files as audio/ogg

--- webp3/conf.py
+++ webp3/conf.py
@@ -10,6 +10,7 @@ MATCH_ETAG = True
 AUDIO_EXTENSIONS = {
 	'.flac': 'audio/flac',
 	'.ogg': 'audio/ogg',
+	'.opus': 'audio/ogg',
 	'.mp3': 'audio/mpeg',
 	'.wav': 'audio/x-wav',
 	'.m4a': 'audio/mpeg'
